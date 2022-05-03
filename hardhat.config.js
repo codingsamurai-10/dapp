8a61a00d2a10dda0978edcc609f586fc46fefc0c
Aditya Rana <official.adityarana01@gmail.com>
2022-05-03 16:49:53 +0530

    .

---
+++ hardhat.config.js
@@ -0,0 +1,35 @@
+require("@nomiclabs/hardhat-waffle");
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+
+  for (const account of accounts) {
+    console.log(account.address);
+  }
+});
+
+// You need to export an object to set up your config
+// Go to https://hardhat.org/config/ to learn more
+
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+module.exports = {
+  solidity: "0.8.4",
+  paths: {
+    artifacts: "./src/artifacts",
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    ropsten: {
+      url: "https://ropsten.infura.io/v3/ede11b819fec41ad9985c66df9efa20c",
+      accounts: [
+        `0x5fa347d4fce0306250275c4cfc547b42681ca7d200d312b12bebcf928dd7effd`,
+      ],
+    },
+  },
+};
--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { ethers } from "ethers";
+import Greeter from "./artifacts/contracts/Greeter.sol/Greeter.json";
+
+const greeterAddress = "0x15d87af420b9f2f44C435026a9339B47F286138d";
 
 function App() {
+  const [greeting, setGreetingValue] = useState("");
+
+  const fetchGreeting = async () => {
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const contract = new ethers.Contract(
+        greeterAddress,
+        Greeter.abi,
+        provider
+      );
+
+      try {
+        const data = await contract.greet();
+        console.log(data);
+      } catch (err) {
+        console.error(err);
+      }
+    }
+  };
+
+  const setGreeting = async () => {
+    if (!greeting) return;
+    if (typeof window.ethereum !== "undefined") {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+      await provider.send("eth_requestAccounts", []);
+
+      const signer = provider.getSigner();
+      const contract = new ethers.Contract(greeterAddress, Greeter.abi, signer);
+      const transaction = await contract.setGreeting(greeting);
+      setGreetingValue("");
+      await transaction.wait();
+      fetchGreeting();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={fetchGreeting}>Fetch greeting</button>
+      <input
+        type="text"
+        onChange={(e) => setGreetingValue(e.target.value)}
+        value={greeting}
+      />
+      <button onClick={setGreeting}>Set greeting</button>
     </div>
   );
 }
